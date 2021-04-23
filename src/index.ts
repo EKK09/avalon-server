fe9511f0ddec9d1ec208cd769558dd54415e6c83
wei <xe856423@gmail.com>
2021-04-23 11:15:02 +0800

    新增跨域請求設定

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express, { Response, Request } from 'express';
+import cors from 'cors';
 import UserModel from './model/userMode';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.get('/user', async (request: Request, response: Response) => {
   const user = await UserModel.getUserName();
